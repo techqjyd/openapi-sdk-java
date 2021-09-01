b9d74d64f2c3757549480e87f0433f75b24e038a
赵鹏程 <zhaopengcheng@qijiayoudao.com>
2021-09-01 15:40:45 +0800

    fix

---
+++ src/main/java/com/xinrenxinshi/response/AttendanceLeaveOutResponse.java
@@ -0,0 +1,22 @@
+package com.xinrenxinshi.response;
+
+/**
+ * 出差打卡列表response
+ *
+ * @author: jiazijie
+ * @create: 2019-11-12 11:45
+ **/
+public class AttendanceLeaveOutResponse {
+    /**
+     * 外出记录ID
+     */
+    private String approvalId;
+
+    public String getApprovalId() {
+        return approvalId;
+    }
+
+    public void setApprovalId(String approvalId) {
+        this.approvalId = approvalId;
+    }
+}
