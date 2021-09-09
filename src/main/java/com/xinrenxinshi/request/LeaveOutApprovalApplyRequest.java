d2ff520245654e706ba81d036ed5ec2b8a54d792
赵鹏程 <zhaopengcheng@qijiayoudao.com>
2021-09-09 11:35:25 +0800

    fix

---
+++ src/main/java/com/xinrenxinshi/request/LeaveOutApprovalApplyRequest.java
@@ -0,0 +1,182 @@
+package com.xinrenxinshi.request;
+
+import com.alibaba.fastjson.TypeReference;
+import com.xinrenxinshi.common.MethodEnum;
+import com.xinrenxinshi.exception.ParamNotValidException;
+import com.xinrenxinshi.openapi.AbstractOpenapiJsonRequest;
+import com.xinrenxinshi.openapi.OpenapiResponse;
+import com.xinrenxinshi.response.ApprovalApplyResponse;
+import com.xinrenxinshi.response.AttendanceLeaveOutResponse;
+import com.xinrenxinshi.util.XRXSStrUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *外出申请审批发起
+ * @author zhaopengcheng
+ */
+public class LeaveOutApprovalApplyRequest extends AbstractOpenapiJsonRequest<ApprovalApplyResponse> {
+    /**
+     * employeeId
+     */
+    private String employeeId;
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 0--按天外出，1–按小时外出
+     */
+    private Integer timeUnit;
+
+    /**
+     * 外出开始日期，日期格式：yyyy-MM-dd HH:mm:ss
+     */
+    private String startDate;
+
+
+    /**
+     * 外出开始日期 按天申请，0：上半天、1：下半天
+     */
+    private Integer startTime;
+
+    /**
+     * 外出开始日期，日期格式：yyyy-MM-dd
+     */
+    private String endDate;
+
+    /**
+     * 出差结束时间，0：上半天、1：下半天
+     */
+    private Integer endTime;
+
+    /**
+     * 申请原因
+     */
+    private String reason;
+
+    public Integer getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void setTimeUnit(Integer timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public Integer getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Integer startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Integer endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LeaveOutApprovalApplyRequest(String accessToken) {
+        super(accessToken);
+    }
+
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    protected Map<String, Object> getParamMap0() {
+        Map<String, Object> map = new HashMap<>(8);
+        map.put("employeeId", employeeId);
+        map.put("mobile", mobile);
+        map.put("timeUnit", timeUnit);
+        map.put("startDate", startDate);
+        map.put("startTime", startTime);
+        map.put("endDate", endDate);
+        map.put("reason", reason);
+        map.put("endTime", endTime);
+        return map;
+    }
+
+    @Override
+    public MethodEnum getMethod() {
+        return MethodEnum.METHOD_POST;
+    }
+
+    @Override
+    public OpenapiResponse<ApprovalApplyResponse> getResponseClass() {
+        return new OpenapiResponse<>();
+    }
+
+    @Override
+    public TypeReference<OpenapiResponse<ApprovalApplyResponse>> getResponseTypeRef() {
+        return new TypeReference<OpenapiResponse<ApprovalApplyResponse>>() {
+        };
+    }
+
+    @Override
+    public void check() throws ParamNotValidException {
+        if (XRXSStrUtils.isEmpty(employeeId) && XRXSStrUtils.isEmpty(mobile)) {
+            throw new ParamNotValidException("员工employeeId和mobile不能同时为空");
+        }
+        if (startDate == null) {
+            throw new ParamNotValidException("外出开始日期为空");
+        }
+        if (endDate == null) {
+            throw new ParamNotValidException("外出结束日期为空");
+        }
+        if (timeUnit == null) {
+            throw new ParamNotValidException("外出申请类型为空");
+        }
+    }
+
+    @Override
+    public String getBizUrl() {
+        return "/v5/workflow/apply/leaveout";
+    }
+}
---
+++ src/main/java/com/xinrenxinshi/response/ApprovalApplyResponse.java
@@ -0,0 +1,24 @@
+package com.xinrenxinshi.response;
+
+/**
+ * <审批发起接口响应结果>.
+ *
+ * @author peng cheng
+ * @date 2021/9/1
+ * @since
+ */
+public class ApprovalApplyResponse {
+
+    /**
+     * 审批id
+     */
+    private Long sid;
+
+    public Long getSid() {
+        return sid;
+    }
+
+    public void setSid(Long sid) {
+        this.sid = sid;
+    }
+}
