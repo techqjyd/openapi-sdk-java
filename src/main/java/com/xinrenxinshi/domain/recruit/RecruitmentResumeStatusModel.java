527977710602a870f3fcd9d7722248e94794d490
huzhuangzhuang <huzhuangzhuang@qijiayoudao.com>
2022-01-19 19:38:42 +0800

    添加上传简历接口

--- src/main/java/com/xinrenxinshi/domain/recruit/RecruitmentResumeStatusModel.java
+++ src/main/java/com/xinrenxinshi/domain/recruit/RecruitmentResumeStatusModel.java
@@ -4,25 +4,6 @@ package com.xinrenxinshi.domain.recruit;
  * @author hzz on 2022/01/14
  */
 public class RecruitmentResumeStatusModel {
-    /**
-     * 简历库
-     */
-    private Integer resumeLibrary;
-
-    /**
-     * 简历库下的分类
-     */
-    private Integer resumeLibraryType;
-
-    /**
-     * 简历状态
-     */
-    private Integer resumeLibraryStatus;
-
-    /**
-     * 简历来源
-     */
-    private Integer sourceType;
 
     /**
      * 投递时间
@@ -40,43 +21,6 @@ public class RecruitmentResumeStatusModel {
      * 渠道名
      */
     private String channelTypeDesc;
-
-
-
-    public Integer getResumeLibrary() {
-        return resumeLibrary;
-    }
-
-    public void setResumeLibrary(Integer resumeLibrary) {
-        this.resumeLibrary = resumeLibrary;
-    }
-
-
-    public Integer getResumeLibraryType() {
-        return resumeLibraryType;
-    }
-
-    public void setResumeLibraryType(Integer resumeLibraryType) {
-        this.resumeLibraryType = resumeLibraryType;
-    }
-
-    public Integer getResumeLibraryStatus() {
-        return resumeLibraryStatus;
-    }
-
-    public void setResumeLibraryStatus(Integer resumeLibraryStatus) {
-        this.resumeLibraryStatus = resumeLibraryStatus;
-    }
-
-
-
-    public Integer getSourceType() {
-        return sourceType;
-    }
-
-    public void setSourceType(Integer sourceType) {
-        this.sourceType = sourceType;
-    }
 
     public Integer getApplyTime() {
         return applyTime;
