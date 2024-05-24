0bb145d2ea5bddd21063787f54c436da0adf9dd7
zhaopc <zhaopengcheng@qijiayoudao.com>
2024-05-24 13:44:23 +0800

    add schedule fields

--- src/main/java/com/xinrenxinshi/domain/attendance/AttendanceClockTimeRanges.java
+++ src/main/java/com/xinrenxinshi/domain/attendance/AttendanceClockTimeRanges.java
@@ -32,6 +32,151 @@ public class AttendanceClockTimeRanges {
      * 打卡最晚时间，格式：HH:mm
      */
     private String clockEndTime;
+    /**
+     * 上班卡开始时间的所属日期 -1: 前一天 0:当天 1:次日
+     */
+    private Integer clockStartEarliestBelong;
+    /**
+     * 上班卡开始时间
+     */
+    private String clockStartEarliestTime;
+    /**
+     * 上班卡结束时间的所属日期 -1: 前一天 0:当天 1:次日
+     */
+    private Integer clockStartLatestBelong;
+    /**
+     * 上班卡结束时间
+     */
+    private String clockStartLatestTime;
+    /**
+     * 下班卡开始时间的所属日期 -1: 前一天 0:当天 1:次日
+     */
+    private Integer clockEndEarliestBelong;
+    /**
+     * 下班卡开始时间
+     */
+    private String clockEndEarliestTime;
+    /**
+     * 下班卡结束时间的所属日期 -1: 前一天 0:当天 1:次日
+     */
+    private Integer clockEndLatestBelong;
+    /**
+     * 下班卡结束时间
+     */
+    private String clockEndLatestTime;
+    /**
+     * 上班豁免是否开启 0-否 1-是
+     */
+    private Integer clockStartExempt;
+
+    /**
+     * 上班豁免时间（分钟）
+     */
+    private Integer clockStartExemptTime;
+    /**
+     * 下班豁免开关 0-关闭 1-开启
+     */
+    private Integer clockEndExempt;
+    /**
+     * 下班豁免时间（分钟）
+     */
+    private Integer clockEndExemptTime;
+
+    public Integer getClockStartExempt() {
+        return clockStartExempt;
+    }
+
+    public void setClockStartExempt(Integer clockStartExempt) {
+        this.clockStartExempt = clockStartExempt;
+    }
+
+    public Integer getClockStartExemptTime() {
+        return clockStartExemptTime;
+    }
+
+    public void setClockStartExemptTime(Integer clockStartExemptTime) {
+        this.clockStartExemptTime = clockStartExemptTime;
+    }
+
+    public Integer getClockEndExempt() {
+        return clockEndExempt;
+    }
+
+    public void setClockEndExempt(Integer clockEndExempt) {
+        this.clockEndExempt = clockEndExempt;
+    }
+
+    public Integer getClockEndExemptTime() {
+        return clockEndExemptTime;
+    }
+
+    public void setClockEndExemptTime(Integer clockEndExemptTime) {
+        this.clockEndExemptTime = clockEndExemptTime;
+    }
+
+    public Integer getClockStartEarliestBelong() {
+        return clockStartEarliestBelong;
+    }
+
+    public void setClockStartEarliestBelong(Integer clockStartEarliestBelong) {
+        this.clockStartEarliestBelong = clockStartEarliestBelong;
+    }
+
+    public String getClockStartEarliestTime() {
+        return clockStartEarliestTime;
+    }
+
+    public void setClockStartEarliestTime(String clockStartEarliestTime) {
+        this.clockStartEarliestTime = clockStartEarliestTime;
+    }
+
+    public Integer getClockStartLatestBelong() {
+        return clockStartLatestBelong;
+    }
+
+    public void setClockStartLatestBelong(Integer clockStartLatestBelong) {
+        this.clockStartLatestBelong = clockStartLatestBelong;
+    }
+
+    public String getClockStartLatestTime() {
+        return clockStartLatestTime;
+    }
+
+    public void setClockStartLatestTime(String clockStartLatestTime) {
+        this.clockStartLatestTime = clockStartLatestTime;
+    }
+
+    public Integer getClockEndLatestBelong() {
+        return clockEndLatestBelong;
+    }
+
+    public void setClockEndLatestBelong(Integer clockEndLatestBelong) {
+        this.clockEndLatestBelong = clockEndLatestBelong;
+    }
+
+    public String getClockEndLatestTime() {
+        return clockEndLatestTime;
+    }
+
+    public void setClockEndLatestTime(String clockEndLatestTime) {
+        this.clockEndLatestTime = clockEndLatestTime;
+    }
+
+    public Integer getClockEndEarliestBelong() {
+        return clockEndEarliestBelong;
+    }
+
+    public void setClockEndEarliestBelong(Integer clockEndEarliestBelong) {
+        this.clockEndEarliestBelong = clockEndEarliestBelong;
+    }
+
+    public String getClockEndEarliestTime() {
+        return clockEndEarliestTime;
+    }
+
+    public void setClockEndEarliestTime(String clockEndEarliestTime) {
+        this.clockEndEarliestTime = clockEndEarliestTime;
+    }
 
     private List<AttendanceRestTimeRange>  restTimeRangs;
 
