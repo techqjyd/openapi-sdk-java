f2d61691d435d64e9ac193f411eddbb327c82005
panhuiwen <panhuiwen@qijiayoudao.com>
2025-05-16 19:00:07 +0800

    add

--- src/main/java/com/xinrenxinshi/XrxsLogService.java
+++ src/main/java/com/xinrenxinshi/XrxsLogService.java
@@ -5,9 +5,7 @@ import com.xinrenxinshi.domain.log.AdminOperationLogResult;
 import com.xinrenxinshi.domain.log.ApiOperationLogResult;
 import com.xinrenxinshi.exception.ApiException;
 import com.xinrenxinshi.openapi.OpenapiResponse;
-import com.xinrenxinshi.request.AdminOperationLogRequest;
-import com.xinrenxinshi.request.ApiOperationLogRequest;
-import com.xinrenxinshi.request.EmployeeDetailInfoListRequest;
+import com.xinrenxinshi.request.*;
 import com.xinrenxinshi.response.PageResult;
 import com.xinrenxinshi.util.RequestTemplate;
 
@@ -19,7 +17,17 @@ public abstract class XrxsLogService {
         return execute;
     }
 
+    public static OpenapiResponse<PageResult<AdminOperationLogResult>> getAdminOperationLogDesensitizedList(AdminOperationLogDesensitizedRequest request) throws ApiException {
+        OpenapiResponse<PageResult<AdminOperationLogResult>> execute = RequestTemplate.execute(request);
+        return execute;
+    }
+
     public static OpenapiResponse<PageResult<ApiOperationLogResult>> getApiOperationLogList(ApiOperationLogRequest request) throws ApiException {
+        OpenapiResponse<PageResult<ApiOperationLogResult>> execute = RequestTemplate.execute(request);
+        return execute;
+    }
+
+    public static OpenapiResponse<PageResult<ApiOperationLogResult>> getApiOperationLogDesensitizedList(ApiOperationLogDesensitizedRequest request) throws ApiException {
         OpenapiResponse<PageResult<ApiOperationLogResult>> execute = RequestTemplate.execute(request);
         return execute;
     }
